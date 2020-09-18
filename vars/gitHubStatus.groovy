43972a34f60969e46729503f3b6848850656e039
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-09-18 09:03:19 -0500

    Initial Commit

---
+++ vars/gitHubStatus.groovy
@@ -0,0 +1,18 @@
+#!/user/bin/env groovy
+
+def call(Map stageParams) {
+    def builder = new groovy.json.JsonBuilder()
+    builder context: stageParams.context, state: stageParams.state, description: stageParams.$description, target_url: stageParams.target_url
+    try {
+      def httpConn = new URL(stageParams.statuses_url).openConnection();
+      httpConn.setRequestMethod("POST");
+      httpConn.setRequestProperty("Authorization", "token ${stageParams.GITHUB_ACCESS_TOKEN}")
+      httpConn.setRequestProperty("Accept", "application/vnd.github.v3+json")
+      httpConn.setRequestProperty("Accept", "application/json");
+      httpConn.setDoOutput(true);
+      httpConn.getOutputStream().write(builder.toString().getBytes("UTF-8"));
+      return httpConn.getResponseCode();
+    } catch(Exception e){
+      echo "Exception: ${e}"
+    }  
+}
