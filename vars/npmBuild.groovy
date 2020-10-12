19abaeb579854b700401ae7117acb3f8ed6fccd9
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-10-11 23:16:00 -0500

    fix some erros

--- vars/npmBuild.groovy
+++ vars/npmBuild.groovy
@@ -6,7 +6,7 @@ def call(Map params) {
       sh(script: "npm run-script build", returnStatus: true)
       def props = readJSON file: 'package.json'
       dir("dist") {
-        sh "tar -czvf ${props.name}-${name.version}.tar.gz ${props.name}/"
+        sh "tar -czvf ${props.name}-${props.version}.tar.gz ${props.name}/"
       }
     } catch(Exception e) {
       echo "Exception: ${e}"
