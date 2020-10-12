8b435ab55e845b6a85a38b88e1eef4fab90679d5
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-10-11 23:00:37 -0500

    change build npm

--- vars/npmBuild.groovy
+++ vars/npmBuild.groovy
@@ -4,6 +4,10 @@ def call(Map params) {
   stage('build artifacts') {
     try {
       sh(script: "npm run-script build", returnStatus: true)
+      def props = readJSON file: 'package.json'
+      dir("dist") {
+        sh "tar -czvf ${props.name}-${name.version}.tar.gz ${props.name}/"
+      }
     } catch(Exception e) {
       echo "Exception: ${e}"
     }
