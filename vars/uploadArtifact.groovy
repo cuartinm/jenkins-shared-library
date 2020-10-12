0b42a7374bdf8a92d2820be8f8bf3ced633d068b
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-10-11 23:07:39 -0500

    change some params

--- vars/uploadArtifact.groovy
+++ vars/uploadArtifact.groovy
@@ -6,7 +6,7 @@ def call(Map params) {
       script: """
         curl -u ${params.username}:${params.password} \
         -X PUT -k "${params.artifactory_url}/${params.repository}/${params.application}/${params.version}/${params.artifact}" \
-        -T ${params.artifact}
+        -T ${params.target}
       """
     )
   }
