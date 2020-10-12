9ea4fa46f5b7db8defeabc93ea2714e030e23825
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-10-12 00:16:30 -0500

    change some

--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -4,7 +4,9 @@ def call(Map params) {
   stage('install packages') {
     try {
       if (params.manager == "npm") {
-        sh(script: "npm i .", returnStatus: true)
+        configFileProvider([configFile(fileId: '17d3db00-8ec7-4598-8b81-b5a4e25a0d2f', variable: 'npm_config_registry')]) {
+          sh(script: "npm i .", returnStatus: true)
+        }
       }
     } catch(Exception e) {
       echo "Exception: ${e}"
