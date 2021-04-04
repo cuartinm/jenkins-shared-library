9cae5e9221fabb16b8b3758297529da769cde532
Miguel Cuartin <macuartin@gmail.com>
2021-04-04 16:36:19 -0500

    fix url

--- vars/bitbucketStatus.groovy
+++ vars/bitbucketStatus.groovy
@@ -4,7 +4,7 @@ def call(Map params) {
   def builder = new groovy.json.JsonBuilder()
   builder key: params.key, state: params.state, description: params.description, url: params.url
   
-  def httpConn = new URL(params.statuses_url).openConnection();
+  def httpConn = new URL(params.url).openConnection();
   httpConn.setRequestMethod("POST");
   httpConn.setRequestProperty("Authorization", "Basic ${params.credentials}")
   httpConn.setRequestProperty("Accept", "application/vnd.github.v3+json")
